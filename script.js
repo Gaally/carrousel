0e33c5dd121b3040904139b2d44b71557a508840
Gaally <tiphainelpr@gmail.com>
2020-09-14 12:14:25 +0200

    modif js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
  // déclaration de variables
  var index = 0;
- var images;
  var time;
 
 
 // initialisation du défilement automatique
  autoSlide();
- function autoSlide(n){
-  images = document.getElementsByClassName('image');
+ function autoSlide(){
+  var images = document.getElementsByClassName('image');
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
 
@@ -25,13 +24,6 @@
   time = setTimeout(autoSlide, 5000);
 }
 
-// initialisation du défilement manuel
-function manualSlide2(n){
-  clearTimeout(time);
-  newIndex = index + n;
-  autoSlide(newIndex);
-}
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -47,16 +39,15 @@ function currentSlide(n) {
 
 // initialisation du menu vignettes et boutons
 function showSlides(n) {
-  var i;
   var slides = document.getElementsByClassName("image");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
+      for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
       }
-      for (i = 0; i < dots.length; i++) {
+      for (var i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace("active", "");
       }
       slides[slideIndex-1].style.display = "block";
